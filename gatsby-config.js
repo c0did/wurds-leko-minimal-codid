833303ffea8343821123d0932f88f7bcb2e6485b
Juno <79345338+co-did@users.noreply.github.com>
2021-02-21 18:43:48 +0800

    removed process string and added GA id

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId: "UA-186203600-1",
       },
     },
     `gatsby-plugin-sitemap`,
