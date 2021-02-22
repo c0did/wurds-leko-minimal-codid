eb76f042945d11403d5d372ba60d9b93d0137d17
Kee Wee <kee@wurds.org>
2021-02-23 02:31:35 +0800

    test title

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
+    title: `WURDS`,
     siteTitleAlt: `Minimal Blog - Gatsby Theme`,
   },
   plugins: [
